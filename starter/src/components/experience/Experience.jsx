eff86ecfc6302403e1adfe2b6b0ef6f067a6c74b
Avraaj Matharu <avr99j@gmail.com>
2024-03-06 10:43:04 +0000

    created the Experience and Projects Components with the CSS styling

--- starter/src/components/experience/Experience.jsx
+++ starter/src/components/experience/Experience.jsx
@@ -1,11 +1,207 @@
 import React from 'react'
 import './experience.css'
+import { BsFillPatchCheckFill } from "react-icons/bs";
+
 
 const Experience = () => {
     return (
         <section id="experience">
             <h5>Skills I have</h5>
             <h2>My Experience</h2>
+
+            <div className="container experience__container">
+                <div className="experience__frontend">
+                    <h3>Development</h3>
+                    <div className="experience__content">
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>HTML</h4>
+                                <small className='' text-light>Experienced</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>CSS</h4>
+                                <small className='' text-light>Intermediate</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>JavaScript</h4>
+                                <small className='' text-light>Experienced</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>Bootstrap</h4>
+                                <small className='' text-light>Experienced</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>ReactJS</h4>
+                                <small className='' text-light>Experienced</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>MongoDB</h4>
+                                <small className='' text-light>Intermediate</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>Express</h4>
+                                <small className='' text-light>Intermediate</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>Node.js</h4>
+                                <small className='' text-light>Intermediate</small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+
+                {/* END OF DEV  */}
+
+                <div className="epxerience__automation">
+                    <h3>Automation Architect</h3>
+                    <div className="experience__content">
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>Frameworks</h4>
+                                <small className='' text-light>Experienced</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>Cypress / WebdriverIO</h4>
+                                <small className='' text-light>Intermediate</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>BDD</h4>
+                                <small className='' text-light>Experienced</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>CI/CD</h4>
+                                <small className='' text-light>Experienced</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>AWS / Ms Azure</h4>
+                                <small className='' text-light>Experienced</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>DevOps</h4>
+                                <small className='' text-light>Experienced</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>Python / Java</h4>
+                                <small className='' text-light>Experienced</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>API</h4>
+                                <small className='' text-light>Experienced</small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+                {/* END OF AUTOMATION */}
+
+                <div className="epxerience__automation">
+                    <h3>AI / ML</h3>
+                    <div className="experience__content">
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>Data Optimisation</h4>
+                                <small className='' text-light>Intermediate</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>Data Visualisation</h4>
+                                <small className='' text-light>Intermediate</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>ML Algorithms</h4>
+                                <small className='' text-light>Beginner</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>AI Models Dev</h4>
+                                <small className='' text-light>Beginner</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>Deep Learning</h4>
+                                <small className='' text-light>Beginner</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>NNs / CNNs</h4>
+                                <small className='' text-light>Beginner</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>Hyperparameters</h4>
+                                <small className='' text-light>Beginner</small>
+                            </div>
+                        </article>
+                        <article className='experience__details'>
+                            <BsFillPatchCheckFill className='experience__details-icon' />
+                            <div>
+                                <h4>SVMs</h4>
+                                <small className='' text-light>Beginner</small>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+
+                {/* END OF AI/ML*/}
+
+            </div>
         </section>
     )
 }
--- starter/src/components/projects/Projects.jsx
+++ starter/src/components/projects/Projects.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 import './projects.css'
+import IMG1 from '../../assets/weatherDash.jpg'
+// import IMG2 from '../../assets/weatherDash.jpg'
+// import IMG3 from '../../assets/weatherDash.jpg'
+// import IMG4 from '../../assets/weatherDash.jpg'
+
 
 const Projects = () => {
     return (
-        <section id='projects'>Projects</section>
+        <section id='projects'>
+            <h5>What I have built</h5>
+            <h2>Projects</h2>
+
+            <div className="container projects__container">
+                <article className='project__item'>
+                    <div className="portfolio__item-image">
+                        <img src={IMG1} alt="Weather Dashboard" />
+                    </div>
+                    <h3>Weather Dashboard</h3>
+                    <a href="https://github.com/Avr99j/weatherDashboard" className='btn' target='_blank'>GitHub</a>
+                    <a href="https://avr99j.github.io/weatherDashboard/" className='btn btn-primary' target='_blank'>Live Demo</a>
+                </article>
+                <article className='project__item'>
+                    <div className="portfolio__item-image">
+                        <img src={IMG1} alt="Weather Dashboard" />
+                    </div>
+                    <h3>Weather Dashboard</h3>
+                    <a href="https://github.com/Avr99j/weatherDashboard" className='btn' target='_blank'>GitHub</a>
+                    <a href="https://avr99j.github.io/weatherDashboard/" className='btn btn-primary' target='_blank'>Live Demo</a>
+                </article>
+                <article className='project__item'>
+                    <div className="portfolio__item-image">
+                        <img src={IMG1} alt="Weather Dashboard" />
+                    </div>
+                    <h3>Weather Dashboard</h3>
+                    <a href="https://github.com/Avr99j/weatherDashboard" className='btn' target='_blank'>GitHub</a>
+                    <a href="https://avr99j.github.io/weatherDashboard/" className='btn btn-primary' target='_blank'>Live Demo</a>
+                </article>
+                <article className='project__item'>
+                    <div className="portfolio__item-image">
+                        <img src={IMG1} alt="Weather Dashboard" />
+                    </div>
+                    <h3>Weather Dashboard</h3>
+                    <a href="https://github.com/Avr99j/weatherDashboard" className='btn' target='_blank'>GitHub</a>
+                    <a href="https://avr99j.github.io/weatherDashboard/" className='btn btn-primary' target='_blank'>Live Demo</a>
+                </article>
+            </div >
+        </section >
     )
 }
 
