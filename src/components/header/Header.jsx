24bc0edc04c9ab42299819d68fbe20fd776a63fe
Avraaj <avr99j@gmail.com>
2024-04-22 18:28:27 +0100

    updated the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
             <div className="container header__container">
                 <h5>Hello I'm</h5>
                 <h1>Avraaj Matharu</h1>
-                <h5 className="text-ligh">Technical Automation Architect | Tech Transformation & Innovation Lead | Cloud DevOps Engineer | AI Evangelist | Leveraging Data Science & ML Algorithms for Insights |</h5>
+                <h5 className="text-ligh">Technical Automation Architect | Tech Transformation & Innovation Lead | Cloud DevOps Engineer | AI Evangelist | Leveraging Data Science & ML Algorithms for Insights</h5>
                 <CTA />
                 <HeaderSocials />
 
